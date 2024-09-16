fc9617237728cabacbff759ac8300bb9f91cca1b
= mlm-games <= gerrit.r6a6a@passinbox.com>
2024-09-16 14:28:47 +0530

    make a arm32 tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,124 +5,83 @@ ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
 
 TARGET_USES_64_BIT_BINDER := true
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := QC_Reference_Phone
-BOARD_VENDOR := qualcomm
-TARGET_SOC := QC_Reference_Phone
-TARGET_BOARD_PLATFORM := msm8953
+TARGET_NO_BOOTLOADER := true
 
 # Kernel
+BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78af000 androidboot.usbconfigfs=true vmalloc=300M loop.max_part=7
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --header_version 2 --board SRPTG29A004
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-#BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo
-BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt/dtb
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
-#TARGET_KERNEL_SOURCE := kernel/samsung/m02s
-#TARGET_KERNEL_CONFIG := sdm450_sec_a02q_swa_ins_defconfig
-#TARGET_KERNEL_CONFIG := $(DEVICE_PATH)kernel/arch/arm64/configs/sdm450_sec_a02q_swa_ins_defconfig
-#TARGET_KERNEL_ARCH := arm64
-#TARGET_KERNEL_HEADER_ARCH := arm64
-#TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-#BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+# Kernel - prebuilt
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8953
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67104768 # 67108864 - 4096 due to orangefox being slightly larger
-BOARD_USES_FULL_RECOVERY_IMAGE := true
-#BOARD_DTBOIMG_PARTITION_SIZE       := 8388608
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_VENDOR := vendor
-BOARD_SUPER_PARTITION_SIZE := 9126805504 # TODO: Fix hardcoded value
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+
+# Dynamic Partitions
+BOARD_SUPER_PARTITION_SIZE := 9126805504
 BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
 BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor product odm
 BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 9122611200
-#BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
-#BOARD_USES_METADATA_PARTITION := true
-#TARGET_COPY_OUT_ODM := odm
 
-# Recovery
-#BOARD_INCLUDE_RECOVERY_DTBO := true
+# File systems
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
-BOARD_ROOT_EXTRA_SYMLINKS := /vendor/dsp:/dsp /vendor/firmware_mnt:/firmware
-
-# Verified Boot
-BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 0
-BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
-BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
-BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 0
-BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 0
-
-# Hack: prevent anti rollback
-PLATFORM_SECURITY_PATCH := 2099-12-31
-VENDOR_SECURITY_PATCH := 2099-12-31
-PLATFORM_VERSION := 16.1.0
+# Recovery
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
+TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
-RECOVERY_SDCARD_ON_DATA := true
-TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_SECONDARY_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
-TW_MAX_BRIGHTNESS := 365
-TW_DEFAULT_BRIGHTNESS := 219
-TW_Y_OFFSET := 79
-TW_H_OFFSET := -79
-TW_HAS_DOWNLOAD_MODE := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-BOARD_USES_QCOM_HARDWARE := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_DEVICE_VERSION := ragebreaker #use your name
-TW_USE_SAMSUNG_HAPTICS := true
-#TW_SCREEN_BLANK_ON_BOOT := true
-#TW_NO_EXFAT_FUSE := true
-#TW_NO_LEGACY_PROPS := true
-#TW_NO_BIND_SYSTEM := true
+TW_Y_OFFSET := 80
+TW_H_OFFSET := -80
+RECOVERY_SDCARD_ON_DATA := true
+TW_INCLUDE_RESETPROP := true
+TW_INCLUDE_REPACKTOOLS := true
+
+# # Crypto
+# TW_INCLUDE_CRYPTO := true
+# TW_INCLUDE_CRYPTO_FBE := true
+# TW_INCLUDE_FBE_METADATA_DECRYPT := true
+
+# Debug flags
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
-TW_INCLUDE_LOGICAL := odm,vendor
 
-#TW_SUPPORT_INPUT_AIDL_MOUSE := true
-#TW_SUPPORT_INPUT_MOUSE := true
-TW_NO_REBOOT_BOOTLOADER := true
-#TW_NO_USB_STORAGE := true
-#TW_EXCLUDE_ENCRYPTED_BACKUPS := true
-#TWRP_EVENT_LOGGING := true
-#TWRP_EXCLUDE_TWRPAPP := true
+# NTFS support
+TW_INCLUDE_NTFS_3G := true
 
-# If its lun.0 ?
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file"
-
-#Try for fixing touch
-# And replace it with:
-#TW_LOAD_VENDOR_MODULES := $(strip $(shell cat $(DEVICE_PATH)/recovery/root/vendor/lib/modules.load))
-#TW_LOAD_VENDOR_BOOT_MODULES := true
-#TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
-
-# System as root
-BOARD_ROOT_EXTRA_FOLDERS := cache carrier efs optics prism omr odm socko odmko keydata keyrefuge
-BOARD_SUPPRESS_SECURE_ERASE := true
-
-# Workaround for error copying vendor files to recovery ramdisk
-#BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-#TARGET_COPY_OUT_VENDOR := vendor
+# Maintainer/Version
+TW_DEVICE_VERSION := mlm-minimal
